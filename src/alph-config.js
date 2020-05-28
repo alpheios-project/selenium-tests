5e05ecb3388f641ea1a0359285392c0b7077ca40
Bridget Almas <balmsa@gmail.com>
2020-05-28 13:59:59 -0400

    misc cleanup work add qa and prod test files configure test file name from environment rename dblclick tests to click

--- src/alph-config.js
+++ src/alph-config.js
@@ -1,7 +1,20 @@
 const config = require('./main-config.json')
 
 module.exports = {
-  versions (env) { 
+
+  testUrl(cfg) {
+    if (!cfg) {
+      cfg = config
+    }
+    let testUrl = cfg.testUrl
+    if (process.env.branch) {
+      testUrl.replace('index.html',`index-${process.env.branch}.html`)
+    }
+    console.info("test url",cfg)
+    return testUrl
+  },
+
+  versions (env) {
     if (!env) {
       env = config.env
     }
@@ -13,24 +26,24 @@ module.exports = {
 
     env.forEach(envItem => {
       let minBrowserVersion
-      
+
       if (envItem.minBrowserVersion) {
         minBrowserVersion = parseInt(envItem.minBrowserVersion)
       }
-      
+
       const localVersions = allVersions.filter(version => {
         let result = true
-  
+
         if (envItem.browserName) {
           result = result && version.browser === envItem.browserName
         }
-  
+
         if (minBrowserVersion) {
           result = result && parseInt(version['browser_version']) >= minBrowserVersion
         } else if (envItem.browserVersions) {
           result = result && envItem.browserVersions.includes(version['browser_version'])
         }
-  
+
         if (envItem.osVersions) {
           result = result && envItem.osVersions.some(envOsVer => envOsVer.os === version.os && envOsVer['os_version'] === version['os_version'])
         }
@@ -43,7 +56,6 @@ module.exports = {
       finalVersions = finalVersions.concat(localVersions)
     })
 
-    
     return finalVersions
   }
 }
